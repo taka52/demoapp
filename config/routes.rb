056a4791364a8d23e92bada44a2b9208b407dcb2
taka52 <taka_omote@berkeley.edu>
2014-12-04 23:20:51 +0000

    fixed homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'static_pages/home'
+root 'static_pages#home'
 
-  get 'static_pages/help'
+match 'help', to: 'static_pages#help', via: 'get'
 
   resources :users
 
-=======
-  root 'static_pages#home'
-  match 'help', to: 'static_pages#help', via: 'get'
-  get 'static_pages/help'
 
->>>>>>> static_pages
+#  root 'static_pages#home'
+ # match 'help', to: 'static_pages#help', via: 'get'
+  #get 'static_pages/help'
+
+#>>>>>>> static_pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
