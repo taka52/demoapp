d9b57cf43fc52ece42d4051054233792eb006dc5
taka52 <taka_omote@berkeley.edu>
2014-12-05 06:10:26 +0000

    small change

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-<<<<<<< HEAD
-=======
     assert_select "h1", "DemoApp"
->>>>>>> static_pages
   end
 
   test "should get help" do
     get :help
     assert_response :success
-<<<<<<< HEAD
-=======
     assert_select "h1", "Help"
->>>>>>> static_pages
   end
 
 end
